e448ad2f2ec93c2e9992d8c7a219c9055f70f175
Nitin Reddy <reddynitin3@gmail.com>
2020-05-16 01:00:43 +0530

    Initialized a flutter app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import './box-game.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  BoxGame game = BoxGame();
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = game.onTapDown;
+  runApp(game.widget);
+  flameUtil.addGestureRecognizer(tapper);
+}
